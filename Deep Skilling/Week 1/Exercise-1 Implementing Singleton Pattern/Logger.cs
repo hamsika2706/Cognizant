387d593f72e675110e0605834dca4cb7251de7e3
Hamsika <hamsikaummaneni@gmail.com>
2026-06-20 17:48:19 +0530

    Add files via upload

---
+++ Deep Skilling/Week 1/Exercise-1 Implementing Singleton Pattern/Logger.cs
@@ -0,0 +1,26 @@
+namespace SingletonPatternExample
+{
+    public class Logger
+    {
+        private static Logger? instance;
+
+        private Logger()
+        {
+            Console.WriteLine("Logger Instance Created");
+        }
+
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine("Log: " + message);
+        }
+    }
+}
