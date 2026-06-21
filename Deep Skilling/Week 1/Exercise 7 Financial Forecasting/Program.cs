3b81c356aa9cf112ad8d1029c4c50f613557ed57
Hamsika <hamsikaummaneni@gmail.com>
2026-06-21 16:47:01 +0530

    Add files via upload

---
+++ Deep Skilling/Week 1/Exercise 7 Financial Forecasting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FinancialForecast
+{
+    class Program
+    {
+        // Recursive method to calculate future value
+        static double FutureValue(double currentValue, double growthRate, int years)
+        {
+            if (years == 0)
+                return currentValue;
+
+            return FutureValue(currentValue * (1 + growthRate), growthRate, years - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Present Value: ");
+            double presentValue = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Annual Growth Rate (in %): ");
+            double growthRate = Convert.ToDouble(Console.ReadLine()) / 100;
+
+            Console.Write("Enter Number of Years: ");
+            int years = Convert.ToInt16(Console.ReadLine());
+
+            double futureValue = FutureValue(presentValue, growthRate, years);
+
+            Console.WriteLine("\nPredicted Future Value: " + futureValue);
+        }
+    }
+}
